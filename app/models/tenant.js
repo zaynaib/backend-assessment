5b0d0cb29743f521b2e4c852411053932069482f
Zaynaib Giwa <zaynaibg@gmail.com>
2020-11-12 01:16:55 -0600

    resolve all merge conflicts

--- app/models/tenant.js
+++ app/models/tenant.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
   'use strict'
   // Makes the Chirp Model available for other files (will also create a table)
 module.exports =  (sequelize, DataTypes) => {
@@ -39,48 +38,3 @@ module.exports =  (sequelize, DataTypes) => {
 
 
 
-
-  
-||||||| 014514e
-=======
-  // Makes the Chirp Model available for other files (will also create a table)
-module.exports =  (sequelize, DataTypes) => {
-      //Creates a "Chirp" model that matches up with DB
-  const Tenant = sequelize.define("tenant",{ 
-
-      tenantID :{
-          type: DataTypes.INTEGER,
-          primaryKey: true,
-          autoIncrement: true,
-      },
-      firstName: {
-        type: DataTypes.STRING,
-        allowNull: false
-      },
-      lastName: {
-        type: DataTypes.STRING,
-        allowNull : false
-      },
-      email: {
-        type: DataTypes.STRING,
-        allowNull: false
-      }
-    });
-
-    
-    Tenant.associate = (models) =>{
-      Tenant.hasOne(models.Unit,{
-        onDelete: "cascade"
-      });
-    };
-
-
-    return Tenant;
-
-  };
-
-
-
-
-  
->>>>>>> 4b3eeba93a90bfb89b88c2856b0aa34f94263179
--- app/routes/api-routes.js
+++ app/routes/api-routes.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 // This may be confusing but here Sequelize (capital) references the standard library
 var Sequelize = require("sequelize");
@@ -16,37 +15,6 @@ const Tenant = require("../models/tenant")(sequelize,Sequelize);
 const Unit = require("../models/unit")(sequelize,Sequelize);
 
 
-
-
-||||||| 014514e
-=======
-
-// module.exports = function(app){
-    
-// app.get('/', (req, res) => {
-//     //res.send('Hello World!')
-//    res.render('index', { title: 'Hey', message: 'Hello there!' })
-
-//   })
-
-// }
-
-// This may be confusing but here Sequelize (capital) references the standard library
-var Sequelize = require("sequelize");
-// sequelize (lowercase) references our connection to the DB.
-var sequelize = require("../config/connection.js");
-
-const db = {};
-
-db.Sequelize = Sequelize;
-db.sequelize = sequelize;
-
-
-
-const Tenant = require("../models/tenant")(sequelize,Sequelize);
-
-
->>>>>>> 4b3eeba93a90bfb89b88c2856b0aa34f94263179
 module.exports = function(app){
     
 app.get('/', (req, res) => {
@@ -54,7 +22,7 @@ app.get('/', (req, res) => {
 
   })
 
-<<<<<<< HEAD
+
   app.get("/api/tenants",function(req,res){
         Tenant.findAll({}).then(function(results) {
       // results are available to us inside the .then
@@ -88,58 +56,7 @@ app.get('/', (req, res) => {
     });
   });
 
-||||||| 014514e
-=======
-  app.get("/api/all",function(req,res){
-        Tenant.findAll({}).then(function(results) {
-      // results are available to us inside the .then
-      res.json(results);
-    });
 
-  })
-
->>>>>>> 4b3eeba93a90bfb89b88c2856b0aa34f94263179
 }
 
 
-// // Routes
-// // =============================================================
-// module.exports = function(app) {
-
-//     app.get('/', (req, res) => {
-//    res.render('index', { title: 'Hey', message: 'Hello there!' })
-
-//   });
-
-//   // Get all chirps
-//   app.get("/api/all", function(req, res) {
-
-//     // Finding all Chirps, and then returning them to the user as JSON.
-//     // Sequelize queries are aynchronous, which helps with percieved speed.
-//     // If we want something to be guaranteed to happen after the query, we'll use
-//     // the .then function
-//     Chirp.findAll({}).then(function(results) {
-//       // results are available to us inside the .then
-//       res.json(results);
-//     });
-
-//   });
-
-//   // Add a chirp
-//   app.post("/api/new", function(req, res) {
-
-//     console.log("Chirp Data:");
-//     console.log(req.body);
-
-//     Chirp.create({
-//       author: req.body.author,
-//       body: req.body.body,
-//       created_at: req.body.created_at
-//     }).then(function(results) {
-//       // `results` here would be the newly created chirp
-//       res.end();
-//     });
-
-//   });
-
-// };
