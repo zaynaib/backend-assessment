4d7223a3ce4b58f91167699582270bcffe17a71d
Zaynaib Giwa <zaynaibg@gmail.com>
2020-11-15 05:14:56 -0600

    error in migrations

---
+++ app/config/config.js
@@ -0,0 +1,25 @@
+require('dotenv').config(); // this is important!
+module.exports = {
+  "development": {
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASS,
+    "database": process.env.DB_DATABASE,
+    "host": process.env.DB_HOST,
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASS,
+    "database": process.env.DB_DATABASE,
+    "host": process.env.DB_HOST,
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "root",
+    "password": null,
+    "database": "database_production",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  }
+
+};
