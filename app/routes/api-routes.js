654799be91d86684e3525f3a89c358f7f87ffffc
Zaynaib Giwa <zaynaibg@gmail.com>
2020-11-15 02:17:19 -0600

    create routes for repair model

--- app/routes/api-routes.js
+++ app/routes/api-routes.js
@@ -6,12 +6,16 @@ var sequelize = require("../config/connection.js");
 
 const Tenant = require("../models/tenant")(sequelize,Sequelize);
 const Unit = require("../models/unit")(sequelize,Sequelize);
+const Repair = require("../models/repair")(sequelize,Sequelize);
 
 //Relations
 Tenant.hasOne(Unit,{
   onDelete: 'SET NULL',
 });
+Tenant.hasMany(Repair);
+
 Unit.belongsTo(Tenant);
+Repair.belongsTo(Tenant);
 
 
 module.exports = function(app){
@@ -38,10 +42,10 @@ app.get('/', (req, res) => {
       where:{id:req.params.id}
 
     }).then(tenant => {
-  res.json(tenant);
-});
+     res.json(tenant);
+    });
 
-});
+    });
   
   //Create Route
   app.post('/api/tenants', (req,res) =>{
@@ -87,6 +91,17 @@ app.get('/', (req, res) => {
         });
       });
 
+      
+  app.get("/api/units/:id",function(req,res){
+    Unit.findAll({
+      where:{id:req.params.id}
+
+    }).then(unit => {
+     res.json(unit);
+    });
+
+    });
+
 
         //Create Route
       app.post('/api/units', (req,res) =>{
@@ -123,5 +138,63 @@ app.get('/', (req, res) => {
       });
     });
 
+      /* -------------------------------------------------------------------------- */
+      /*                                Repair Routes                                 */
+      /* -------------------------------------------------------------------------- */
+
+      app.get("/api/repairs",function(req,res){
+        Repair.findAll({}).then( repairs=> {
+        res.json(repairs);
+        });
+      });
+
+      
+    app.get("/api/repairs/:id",function(req,res){
+      Repair.findAll({
+        where:{id:req.params.id}
+
+      }).then(repair => {
+      res.json(repair);
+      });
+
+      });
+
+
+        //Create Route
+      app.post('/api/repairs', (req,res) =>{
+        Repair.create(req.body).then(newRepair =>{res.json(newRepair)})
+      });
+
+
+       //Delete Route
+  app.delete("/api/repairs/:id", (req, res)=> {
+    Repair.destroy({
+      where: {
+        id: req.params.id
+      }
+    }).then(deletedRepair =>{
+      res.json(deletedRepair);
+    });
+  });
+
+  //Update Route
+    app.put("/api/repairs/:id", (req, res) =>{
+      // Update takes in an object describing the properties we want to update, and
+      // we use where to describe which objects we want to update
+      Repair.update({
+        tenantId: req.body.tenantId,
+        note:req.body.note,
+        type:req.body.type,
+        complete:req.body.complete
+      }, {
+        where: {
+          id: req.params.id
+        }
+      }).then(updatedRepair =>{
+        res.json(updatedRepair);
+      });
+    });
+
+      
 
 }
