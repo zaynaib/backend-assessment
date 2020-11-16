c9b8c2019d7d987f3bfb348c51daad892f8a476b
Zaynaib Giwa <zaynaibg@gmail.com>
2020-11-16 04:45:06 -0600

    add associations to repair and unit models

--- models/unit.js
+++ models/unit.js
@@ -11,6 +11,8 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      Unit.belongsTo(models.Tenant);
+
     }
   };
   Unit.init({
